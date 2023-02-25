de048c56a2478b656d315494844927b2de0f0959
castanedaLuis <castanedaLuis@github.com>
2023-02-25 11:22:22 -0600

    [ 🧬 ] Metodos Generics

--- src/base-http.service.ts
+++ src/base-http.service.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { Product } from './app/models/product.model'
 
+
+import { Category } from './app/models/category.model';
+
+
 export class BaseHttpService<T> {
 
     constructor(
@@ -11,13 +15,23 @@ export class BaseHttpService<T> {
       const { data } = await axios.get<T[]>(this.url);
       return data;
     }
+
+    // <ID, DTO>  Es una forma de decirle que tipo de dato va a recibir la función o que que tipo va a manejar para que no infiera 
+    async update<ID, DTO>(id: ID, changes: DTO) {
+        const { data } = await axios.put(`${this.url}/${id}`, changes);
+        return data;
+      }
   }
 
 
-  const service = new BaseHttpService<number>('https://localhost');
+  const service = new BaseHttpService<string>('https://localhost');
 
   service.getAll();
+
   
 
   const serviceProduct = new BaseHttpService<Product>('https://localhost');
   serviceProduct.getAll();
+  serviceProduct.update<Product['id'], Object>(1, {
+    title: 'asa',
+  });
